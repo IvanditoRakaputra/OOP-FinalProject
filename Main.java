28141e90aca75b1384820ce99283a052c430eef3
IvanditoRakaputra <114067618+IvanditoRakaputra@users.noreply.github.com>
2023-06-16 18:43:17 +0700

    Delete Main.java

--- Main.java
+++
@@ -1,6 +0,0 @@
-// This is the main method that is automatically executed when the program starts.
-public class Main {
-    public static void main(String[] args) {
-        new GameGUI();
-    }
-}
